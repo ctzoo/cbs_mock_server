b50c90491b20fc74d032a308490c6a548f0b7eb7
cuitao <cuitaocrazy@163.com>
2018-05-23 01:59:57 +0800

    add basic auth

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const dom = require('xmldom').DOMParser
 const socketIo = require('socket.io')
 const http = require('http')
 const events = require('events')
+const basicAuth = require('express-basic-auth')
 
 const evt = new events.EventEmitter()
 
@@ -30,41 +31,47 @@ namespaceReq.on('connection', socket => {
     tmp[msg.key].res.send(msg.res)
   })
 })
-app.use(bodyParser.text())
+app.use(bodyParser.text({ type: 'text/html' }))
 
 const s = (p, n) => xpath.select1(p, n)
 const ss = (p, n) => xpath.select1(p, n).textContent
 
 app.use(express.static('./'))
-app.get('/cbs', (req, res) => {
-  const doc = new dom().parseFromString(req.body)
-  const cbsHeader = s('/REQUEST/MESSAGE/HEADER', doc)
-  const cbsEnquiry = s('/REQUEST/MESSAGE/ENQUIRY', doc)
-  const cbsConsumer = s('APPLICANT/CONSUMER', cbsEnquiry)
-  const cbsReq = {
-    clientId: ss('CLIENT_ID', cbsHeader),
-    userId: ss('USER_ID', cbsHeader),
-    runNo: ss('RUN_NO', cbsHeader),
-    accountType: ss('ENQUIRY_TYPE', cbsEnquiry),
-    amount: ss('AMOUNT', cbsEnquiry),
-    enquiryReference: ss('ENQUIRY_REFERENCE', cbsEnquiry),
-    enquiryType: ss('ENQUIRY_TYPE', cbsEnquiry),
-    productType: ss('PRODUCT_TYPE', cbsEnquiry),
-    idType: ss('CID/CID1', cbsConsumer),
-    idNumber: ss('CID/CID2', cbsConsumer),
-    customerName: ss('CNAM/CNMU', cbsConsumer),
-    dayOfBirth: ss('CDOB/DBD', cbsConsumer),
-    monthOfBirth: ss('CDOB/DBM', cbsConsumer),
-    yearOfBirth: ss('CDOB/DBY', cbsConsumer),
-    gender: ss('CGND', cbsConsumer),
-    maritalStatus: ss('CMAR', cbsConsumer),
-    applicantType: ss('APPLICANT/CTYP', cbsEnquiry),
-    addressType: ss('CADR/CADT', cbsConsumer),
-    addressFormat: ss('CADR/CADF', cbsConsumer),
-    postalCode: ss('CADR/CAD7', cbsConsumer),
+app.post(
+  '/cbs',
+  basicAuth({
+    users: { cuitao: 'cuitaopwd' },
+  }),
+  (req, res) => {
+    const doc = new dom().parseFromString(req.body)
+    const cbsHeader = s('/REQUEST/MESSAGE/HEADER', doc)
+    const cbsEnquiry = s('/REQUEST/MESSAGE/ENQUIRY', doc)
+    const cbsConsumer = s('APPLICANT/CONSUMER', cbsEnquiry)
+    const cbsReq = {
+      clientId: ss('CLIENT_ID', cbsHeader),
+      userId: ss('USER_ID', cbsHeader),
+      runNo: ss('RUN_NO', cbsHeader),
+      accountType: ss('ENQUIRY_TYPE', cbsEnquiry),
+      amount: ss('AMOUNT', cbsEnquiry),
+      enquiryReference: ss('ENQUIRY_REFERENCE', cbsEnquiry),
+      enquiryType: ss('ENQUIRY_TYPE', cbsEnquiry),
+      productType: ss('PRODUCT_TYPE', cbsEnquiry),
+      idType: ss('CID/CID1', cbsConsumer),
+      idNumber: ss('CID/CID2', cbsConsumer),
+      customerName: ss('CNAM/CNMU', cbsConsumer),
+      dayOfBirth: ss('CDOB/DBD', cbsConsumer),
+      monthOfBirth: ss('CDOB/DBM', cbsConsumer),
+      yearOfBirth: ss('CDOB/DBY', cbsConsumer),
+      gender: ss('CGND', cbsConsumer),
+      maritalStatus: ss('CMAR', cbsConsumer),
+      applicantType: ss('APPLICANT/CTYP', cbsEnquiry),
+      addressType: ss('CADR/CADT', cbsConsumer),
+      addressFormat: ss('CADR/CADF', cbsConsumer),
+      postalCode: ss('CADR/CAD7', cbsConsumer),
+    }
+    tmp[cbsReq.runNo] = { req: cbsReq, res }
+    evt.emit('req', { key: cbsReq.runNo, req: cbsReq })
   }
-  tmp[cbsReq.runNo] = { req: cbsReq, res }
-  evt.emit('req', { key: cbsReq.runNo, req: cbsReq })
-})
+)
 
 httpServer.listen(3001, () => {})
